29bbda4b7475cc7dece9c203cd4d2a1cba0d0b4b
Mariengs <marianne.waago@hotmail.com>
2025-08-24 14:54:27 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  // viktig for GitHub Pages – lokalt har det ingen effekt:
-  base: "/developer1.0/",
+  base: "/", //netlify
 });
